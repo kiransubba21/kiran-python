6b44d123f36e9a2dc6ed217b65ab5ef5dc8377c0
kiran subba <klimboo85@gmail.com>
2023-05-22 13:02:29 -0700

    comparision operater.ipynb

---
+++ area.py
@@ -0,0 +1,4 @@
+h = float (input("enter the width"))
+w = float (input("enter the height "))
+area = w * h
+print("this is the result: ",area)
---
+++ pi.py
@@ -0,0 +1,5 @@
+pi = 3.14
+r=float(input("enter the radius "))
+h=float(input("enter the height "))
+volume = pi*(r**2)*h
+print("the result is ",volume)
